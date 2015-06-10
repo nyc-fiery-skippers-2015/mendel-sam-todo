3cd44b856cc4485e2792e8789c5806154cf98cd6
Mendel Schneerson & Samantha Belkin <pair+benlights.sbelkin88@devbootcamp.com>
2015-06-10 15:37:31 -0400

    ListsControllerSpec#new: basic route test and content test

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -26,4 +26,18 @@ describe ListsController do
     end
   end
 
+  describe 'GET#new' do
+    it 'is successful' do
+      get :new
+      expect(response).to be_success
+    end
+
+    it 'assigns @list to a new list' do
+      get :new
+      expect(assigns(:list)).to be_a_new List
+    end
+
+  end
+
+
 end
