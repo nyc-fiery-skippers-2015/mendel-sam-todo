d6a4031416f3af1073434385f891f5c1ce83ce23
Mendel Schneerson & Samantha Belkin <pair+benlights.sbelkin88@devbootcamp.com>
2015-06-10 18:12:48 -0400

    Task#create method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,19 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @list = List.find_by(id: params[:list_id])
+    @task = @list.tasks.build(task_params)
+    if @task.save
+      redirect_to list_task_path(@list, @task)
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:body)
+  end
+
 end
