375122745e3e32ad72f812d22c130f47d9ffa124
Mendel Schneerson & Samantha Belkin <pair+benlights.sbelkin88@devbootcamp.com>
2015-06-10 11:31:05 -0400

    added list model tests

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
-
-
-describe List do
+describe 'canary test' do
   it 'should work' do
     expect(true).to eq(true)
+  end
+end
+describe List do
+  let(:list) {List.new(title: "test")}
+
+  it 'is valid with a title' do
+    expect(list).to be_valid
+  end
+
+  it 'is invalid without a title' do
+    list2 = List.new(title: nil)
+    list2.valid?
+    expect(list2.errors[:title]).to include("can't be blank")
   end
 end
