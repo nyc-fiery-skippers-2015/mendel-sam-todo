d8d9762cfbea8400f32aa8a0eafe60957c039cab
Mendel Schneerson & Samantha Belkin <pair+benlights.sbelkin88@devbootcamp.com>
2015-06-10 15:56:49 -0400

    ListController#create implement method

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,4 +10,20 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
