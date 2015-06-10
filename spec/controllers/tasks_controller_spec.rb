79adeecef981eb5d55514b009345bc7be7451e75
Mendel Schneerson & Samantha Belkin <pair+benlights.sbelkin88@devbootcamp.com>
2015-06-10 17:54:19 -0400

    TasksController#new basic route and content tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,18 +2,32 @@ require 'rails_helper'
 
 describe TasksController do
   let(:list) {List.create(title: "test")}
+  let(:task) {list.tasks.build(body: "test body")}
   describe 'GET#show' do
-    let(:task) {list.tasks.build(body: "test body")}
     it 'is successful' do
       task.save
       get :show, list_id: list.id, id: task.id
       expect(response).to be_success
     end
 
-    it 'assigns @tasks to Task found by id' do
+    it 'assigns @task to Task found by id' do
       task.save
       get :show, list_id: list.id, id: task.id
       expect(assigns(:task)).to eq task
     end
   end
+
+  describe 'GET#new' do
+    it 'is successful' do
+      get :new, list_id: list.id
+      expect(response).to be_success
+    end
+
+    it 'assigns @task to a task' do
+      get :new, list_id: list.id
+      expect(assigns(:task)).to be_a_new Task
+    end
+
+  end
+
 end
