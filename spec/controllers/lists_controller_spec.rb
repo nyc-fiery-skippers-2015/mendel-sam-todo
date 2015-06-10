fd7de86bf8f311af92a67da32dbe1cd106d044cb
Mendel Schneerson & Samantha Belkin <pair+benlights.sbelkin88@devbootcamp.com>
2015-06-10 12:23:33 -0400

    ListsControllerSpec#show: basic route test and content test

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -11,6 +11,19 @@ describe ListsController do
       get :index
       expect(assigns(:lists)).to eq List.all
     end
-
   end
+
+  describe 'GET#show' do
+    let(:list) {List.create(title: "test")}
+    it 'is successful' do
+      get :show, id: list.id
+      expect(response).to be_success
+    end
+
+    it 'assigns @lists to the List found by id' do
+      get :show, id: list.id
+      expect(assigns(:list)).to eq list
+    end
+  end
+
 end
